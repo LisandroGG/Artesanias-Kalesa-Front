5a263879a62b14efe6c5fb7ffbd91f28a7c9080f
Lisandro Pereyra <lisandro.pereyra123456@gmail.com>
2024-06-26 11:09:54 -0300

    fix change

--- src/components/JobList/JobList.jsx
+++ src/components/JobList/JobList.jsx
@@ -1,7 +1,7 @@
 import style from '../JobList/JobList.module.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { useEffect } from 'react'
-import { getTrabajos, deleteTrabajo } from '../../redux/actions'
+import { getTrabajos } from '../../redux/actions'
 
 const JobList = () => {
 
@@ -11,19 +11,13 @@ const JobList = () => {
     useEffect(() =>{
         dispatch(getTrabajos())
     }, [dispatch])
-
-    const handleDelete = (id) => {
-        dispatch(deleteTrabajo(id))
-        window.location.reload()
-    }
     
     return(
         <div>
             {trabajos.map(trabajo => <div key={trabajo.id} className={style.card}>
                     <h3>{trabajo.nombre}</h3>
                     <p>{trabajo.descripcion}</p>
-                    {trabajo.imagen && <img src={`${import.meta.env.VITE_HOST}${trabajo.imagen}`} alt={trabajo.nombre} loading="lazy" />}
-                    <button onClick={() => handleDelete(trabajo.id)}>Borrar</button>
+                    {trabajo.imagen && <img src={trabajo.imagen} alt={trabajo.nombre} />}
                 </div>)}
         </div>
     )
