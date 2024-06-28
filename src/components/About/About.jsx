a2dfa7db4d36bdb80e41d755bd2c24a412293c4b
Lisandro Pereyra <lisandro.pereyra123456@gmail.com>
2024-06-28 16:05:27 -0300

    pagina terminada

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,7 +5,17 @@ const About = () => {
 
     return(
         <div className={style.about}>
-        <h1>Hola soy about</h1>
+            <div className={style.content}>
+                <h2>¡Hola soy Kalesa!</h2>
+                <br />
+                <p>¡Hola! Soy Karina, y me apasiona ser artesana en porcelana fría desde hace más de 15 años, aquí en Leones, provincia de Córdoba, Argentina. Para mí, cada pieza que creo es mucho más que un simple adorno; es el resultado de años de dedicación, paciencia y amor por mi trabajo.</p>
+                <br />
+                <p>Para mí, ser artesana no es solo un trabajo, es una forma de vida que me permite expresarme y conectar con quienes aprecian el arte en todas sus formas.</p>
+                <br />
+                <p>En mi taller, cada día es una nueva oportunidad para transformar simples materiales en obras de arte únicas y llenas de significado. Mis creaciones, desde adornos para tortas hasta souvenirs y centros de mesa, reflejan no solo mi habilidad técnica, sino también mi creatividad y pasión por cada detalle.</p>
+                <br />
+        
+            </div>
         </div>
     )
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return(
         <div className={style.footer}>
             <div className={style.content}>
-            <p>© 2024 <span>Artesanias Kalesa</span> <br></br> <a href="https://lisandrodev.vercel.app/">Diseño web <span>Lisandro Pereyra</span></a></p>
+            <p>© 2024 <span>Artesanias Kalesa</span> <br></br> <a href="https://lisandrodev.vercel.app/" target='_blank'>Diseño web <span>Lisandro Pereyra</span></a></p>
             </div>
 
         </div>
