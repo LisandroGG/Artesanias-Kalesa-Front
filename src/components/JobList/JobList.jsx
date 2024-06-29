a90904804899baaaef01237c0072a45f2c545897
Lisandro Pereyra <lisandro.pereyra123456@gmail.com>
2024-06-28 22:15:52 -0300

    loading

--- src/components/JobList/JobList.jsx
+++ src/components/JobList/JobList.jsx
@@ -1,17 +1,29 @@
 import style from '../JobList/JobList.module.css'
 import { useDispatch, useSelector } from 'react-redux'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { getTrabajos } from '../../redux/actions'
 
 const JobList = () => {
 
     const dispatch = useDispatch()
     const trabajos = useSelector((state) => state.trabajos)
+    const [loading, setLoading] = useState(true);
 
-    useEffect(() =>{
-        dispatch(getTrabajos())
-    }, [dispatch])
-    
+    useEffect(() => {
+        const fetchTrabajos = async () => {
+            setLoading(true);
+            await dispatch(getTrabajos());
+            setLoading(false);
+        };
+
+        fetchTrabajos();
+    }, [dispatch]);
+
+    if (loading) {
+        return <div className={style.loading}>
+                    <img src="/loading.gif" alt="Cargando"></img>
+                </div>;
+    }
     return(
         <div className={style.cards}>
             {trabajos.map(trabajo => <div key={trabajo.id} className={style.card}>
